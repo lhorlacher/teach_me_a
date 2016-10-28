fc37bd47e7bc8046ca3a80b17ac1a5d27a92ac3f
Lonnie Horlacher <horlacher.lonnie@gmail.com>
2016-10-28 14:44:03 -0600

    default to users teacher field

--- db/migrate/20161025220950_create_users.rb
+++ db/migrate/20161025220950_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.boolean :teacher, default: true
-      t.boolean :teacher_id
+      t.boolean :teacher, default: true, null: false
+      t.integer :teacher_id
 
       t.timestamps
     end
