e1c8f56dd9ac005bf568aa4624dfb74cdc1d0933
Ryan Horlacher <horlacherr@gmail.com>
2016-11-01 10:52:04 -0600

    fixed current_user error on layout

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,28 +1,41 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
-$(document).ready(function() {
-	fields = $('.form-control')
+$(document).on('turbolinks:load', function() {
+	var changes = false
+	var fields = $('.form-control')
 
 	for(var i = 0; i < fields.length; i++) {
 		setReadOnlyTrueListener(fields[i])
 	}
 
 	function setReadOnlyTrueListener(field) {
-		console.log('click event added to:')
-		console.log(field)
 		field.addEventListener('dblclick', function(e){
+			console.log('clicked')
 			var field = e.target
 			field.readOnly = false
 			field.focus()
-			setReadOnlyFalseListener(field)
+			var originalValue = field.value
+			setReadOnlyFalseListener(field, originalValue)
 		})
 	}
 
-	function setReadOnlyFalseListener(field) {
+	function setReadOnlyFalseListener(field, originalValue) {
 		field.addEventListener('blur', function(e){
-			e.target.readOnly = true
+			var input = e.target
+			input.readOnly = true
+			if (originalValue !== input.value) {
+				changes = true
+			}
 		})
 	}
+	function confirmLeave() {
+		console.log('event is firing')
+		if(changes === true) {
+			changes === false
+			return 'You have unsaved changes!'
+		}
+	}
+	window.onbeforeunload = function(){
+		console.log('this is happening')
+  return 'Are you sure you want to leave?';
+};
 
 })
