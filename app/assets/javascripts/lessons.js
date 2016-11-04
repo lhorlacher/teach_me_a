1b17e8c6e5d9bd1a41a6a8150e4f72dd7eda05c0
Ryan Horlacher <horlacherr@gmail.com>
2016-11-04 13:31:35 -0600

    colors and practices fill on assignments

--- app/assets/javascripts/lessons.js
+++ app/assets/javascripts/lessons.js
@@ -1,2 +1,21 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function() {
+	console.log('this is happening')
+
+	var fillDivs = $('.fill-div')
+	var colors = ['#66ffff', '#66ff66', '#ff9933', '#6699ff']
+
+	fillDivs.map(function(i) {
+		var div = fillDivs[i]
+		setHeight(div)
+		setColor(div, i)
+	})
+
+	function setHeight(div) {
+		div.style.height = div.dataset.fill + '%'
+	}
+
+	function setColor(div, i) {
+		div.style.backgroundColor = colors[i % colors.length]
+	}
+
+})
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,11 @@
 class Assignment < ApplicationRecord
 	belongs_to :lesson
+	has_many :practices
 	validates_presence_of :book, :page
+
+	def fill
+		count = self.practices.count
+		needed = self.per_day * self.per_week
+		percent = (count.to_f / needed.to_f * 100).to_i
+	end
 end
